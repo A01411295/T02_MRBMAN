7482c25fcfa7cff2f26069d4a999cd9868b63cc8
Esteban Azuara Aguirre <A01410412@itesm.mx>
2018-04-19 17:09:41 -0500

    ProyectoFinal

---
+++ Assets/Scripts/Rayo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rayo : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D co)
+    {
+        if (co.name == "Lamb")
+            Destroy(gameObject);
+    }
+}
+
